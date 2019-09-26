0556edd8a50f6292623751977d388983b6179557
Merlin Webster <mwebster@witekio.com>
2019-09-26 14:46:07 +0100

    Added basic Jinja2 templating

--- aiohttp/server-example.py
+++ aiohttp/server-example.py
@@ -1,16 +1,18 @@
 # Modified from https://aiohttp.readthedocs.io/en/stable/
 from aiohttp import web
+from jinja2 import Template
 
 
 # aiohttp
 app = web.Application()
 routes = web.RouteTableDef()
 
+
 @routes.get('/')
 @routes.get('/{name}')
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
-    text = 'Hello ' + name + '!'
+    text = Template('Hello {{name}}!').render(name=name)
     return web.Response(text=text)
 
 
